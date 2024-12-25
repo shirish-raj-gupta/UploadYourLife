f582cb608442625333122668ec0648600b2dd53f
Puskar-Roy <puskarroy600@gmail.com>
2024-12-25 21:25:09 +0530

    feat : frontend

---
+++ apps/client/app/page.tsx
@@ -0,0 +1,102 @@
+import Image, { type ImageProps } from "next/image";
+import { Button } from "@repo/ui/button";
+import styles from "./page.module.css";
+
+type Props = Omit<ImageProps, "src"> & {
+  srcLight: string;
+  srcDark: string;
+};
+
+const ThemeImage = (props: Props) => {
+  const { srcLight, srcDark, ...rest } = props;
+
+  return (
+    <>
+      <Image {...rest} src={srcLight} className="imgLight" />
+      <Image {...rest} src={srcDark} className="imgDark" />
+    </>
+  );
+};
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <ThemeImage
+          className={styles.logo}
+          srcLight="turborepo-dark.svg"
+          srcDark="turborepo-light.svg"
+          alt="Turborepo logo"
+          width={180}
+          height={38}
+          priority
+        />
+        <ol>
+          <li>
+            Welcome To <code>UploadYourLife!</code>
+          </li>
+          <li>Save and see your changes instantly.</li>
+        </ol>
+
+        <div className={styles.ctas}>
+          <a
+            className={styles.primary}
+            href="https://github.com/Puskar-Roy/UploadYourLife"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className={styles.logo}
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={20}
+              height={20}
+            />
+            Connect With Us
+          </a>
+          <a
+            href="https://github.com/Puskar-Roy/UploadYourLife"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.secondary}
+          >
+            Read our docs
+          </a>
+        </div>
+        <Button appName="docs" className={styles.secondary}>
+          Open alert
+        </Button>
+      </main>
+      <footer className={styles.footer}>
+        <a
+          href="https://github.com/Puskar-Roy/UploadYourLife"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Github
+        </a>
+        <a
+          href="https://puskarroy.in"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to puskarroy.in →
+        </a>
+      </footer>
+    </div>
+  );
+}
